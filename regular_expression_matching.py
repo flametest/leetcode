702bee3c7395138718adef30064c258e84159d56
Jun <flametest@gmail.com>
2016-04-11 16:41:26 +0800

    10. Regular Expression Matching

---
+++ regular_expression_matching.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2016-04-11 16:30:31
+# @Author  : jun.jiang (flametest@gmail.com)
+# @Link    : http://www.qunar.com
+# @Version : $Id$
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
