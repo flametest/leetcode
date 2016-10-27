4f826f77c595f8b0df23240a761a539184787344
Jun <flametest@gmail.com>
2016-10-27 14:29:53 +0800

    371. Sum of Two Integers new

--- sum_of_two_integers.py
+++ sum_of_two_integers.py
@@ -13,15 +13,25 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
+
+        MAX = 0x7FFFFFFF
+        #MIN = 0x80000000
+        MASK = 0xFFFFFFFF
         if a == 0:
-            return b
+            if b > MAX:
+                return ~(b ^ MASK)
+            else:
+                return b
         if b == 0:
-            return a
-        x = a ^ b #算出相加结果，不含进位
-        y = a & b #算出进位
-        z = y << 1 #移位相加
+            if a > MAX:
+                return ~(a ^ MASK)
+            else:
+                return a
+        x = (a ^ b) & MASK#算出相加结果，不含进位
+        y = (a & b) & MASK #算出进位
+        z = (y << 1) & MASK #移位相加
         return self.getSum(x, z)
 
 
 if __name__ == '__main__':
-    print Solution().getSum(1, 6)
+    print Solution().getSum(-1, 0)
