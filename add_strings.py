2fedf188f56144d0642cfef3abb0b394502a7d9c
Jun <flametest@gmail.com>
2016-11-04 15:51:09 +0800

    fix

--- add_strings.py
+++ add_strings.py
@@ -12,7 +12,16 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        return str(int(num1) + int(num2))
+        return str(self.get_int(num1) + self.get_int(num2))
+
+    def get_int(self, s_num):
+        sum = 0
+        for k, i in enumerate(reversed(s_num)):
+            sum += (ord(i) - ord("0")) * 10 ** k
+        return sum
+
+
 
 if __name__ == '__main__':
+    print Solution().get_int("89")
     print Solution().addStrings("8", "9")
