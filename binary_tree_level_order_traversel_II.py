e31fc0f96e718768e9c57ab122a8e29e44be901b
Jun Jiang <flametest@gmail.com>
2017-09-13 19:17:22 +0800

    102. Binary Tree Level Order Traversal

--- binary_tree_level_order_traversel_II.py
+++ binary_tree_level_order_traversel_II.py
@@ -21,26 +21,27 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        
         result = []
+        if not root:
+            return result
         import Queue
         q = Queue.Queue()
         q.put(root)
-        q.put("r")
-        row = []
-        while q.qsize():
-            e = q.get()
-            if e == "r":
-                if row:
-                    result.append(row)
-                row = []
-                continue
-            row.append(e.val)
-            if e.left:
-                q.put(e.left)
-            if e.right:
-                q.put(e.right)
-            q.put("r")
-        return result[::-1]
+        while True:
+            row = []
+            while not q.empty():
+                e = q.get()
+                row.append(e)
+            for j in row:
+                if j.left:
+                    q.put(j.left)
+                if j.right:
+                    q.put(j.right)
+            result.append([i.val for i in row])
+            if q.empty():
+                break
+        return result
 
 
 if __name__ == '__main__':
@@ -50,3 +51,12 @@ if __name__ == '__main__':
     a.right.left = TreeNode(15)
     a.right.right = TreeNode(7)
     print Solution().levelOrderBottom(a)
+    print Solution().levelOrderBottom(None)
+    b = TreeNode(5)
+    print Solution().levelOrderBottom(b)
+    c = TreeNode(1)
+    c.left = TreeNode(2)
+    c.right = TreeNode(3)
+    c.left.left = TreeNode(4)
+    c.right.right = TreeNode(5)
+    print Solution().levelOrderBottom(c)
