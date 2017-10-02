3a98ad63717d593da5a9dc06220e1357e96a06cb
Jun Jiang <flametest@gmail.com>
2017-10-02 15:46:41 +0800

    563. Binary Tree Tilt

--- binary_tree_tilt.py
+++ binary_tree_tilt.py
@@ -20,11 +20,27 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        if not root:
-            return 0
-        return abs(self.node_sum(root.left) - self.node_sum(root.right))
+        result = []
+        return sum(self.helper(root, result))
+
+    def helper(self, node, result=[]):
+        tilt = 0
+        if not node:
+            result.append(0)
+            return result
+        if node.left:
+            self.helper(node.left, result)
+        if node.right:
+            self.helper(node.right, result)
+        sum_left = self.node_sum(node.left)
+        sum_right = self.node_sum(node.right)
+        tilt = abs(sum_left - sum_right)
+        result.append(tilt)
+        return result
 
     def node_sum(self, node):
+        if not node:
+            return 0
         if node.left is None and node.right is None:
             return node.val
         if node.left is None and node.right:
@@ -35,9 +51,19 @@ class Solution(object):
 
 
 if __name__ == '__main__':
+    print Solution().findTilt(None)
     a = TreeNode(1)
     a.left = TreeNode(2)
     a.right = TreeNode(3)
     # a.left.left = TreeNode(4)
     print Solution().node_sum(a)
     print Solution().findTilt(a)
+    b = TreeNode(1)
+    b.left = TreeNode(2)
+    print Solution().findTilt(b)
+    c = TreeNode(1)
+    c.left = TreeNode(2)
+    c.left.left = TreeNode(4)
+    c.right = TreeNode(3)
+    c.right.left = TreeNode(5)
+    print Solution().findTilt(c)
