ae68d0f9d1ba95025662695d224f0d36b4555d52
Jun Jiang <flametest@gmail.com>
2017-10-02 16:24:44 +0800

    322. Coin Change

---
+++ coin_change.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2017-09-29 09:55:15
+# @Author  : Jun Jiang (flametest@gmail.com)
+# @Link    : http://example.org
+# @Version : $Id$
+
+
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [0] + [amount + 1] * amount
+        for i in xrange(1, len(dp)):
+            for coin in coins:
+                if i >= coin:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        return [dp[amount], -1][dp[amount] == amount + 1]
+
+
+
+if __name__ == '__main__':
+    print Solution().coinChange([1, 2, 5], 11)
+    print Solution().coinChange([], 11)
+    print Solution().coinChange([1, 2, 5], 0)
+    print Solution().coinChange([2], 1)
+    print Solution().coinChange([186, 419, 83, 408], 6249)
+ 
