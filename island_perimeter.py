7b8a14978877f220bd39276e895146ae8463561a
Jun Jiang <flametest@gmail.com>
2017-10-03 12:11:55 +0800

    463. Island Perimeter

--- island_perimeter.py
+++ island_perimeter.py
@@ -12,34 +12,22 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        land_cell = 0
         perimeter = 0
-        # print grid[-1][-1]
         for i, row in enumerate(grid):
             for j, element in enumerate(row):
                     if element == 1:
-                        land_cell += 1
-                        perimeter += self.count_zero(grid, i, j)
+                        perimeter += self.count_edge(grid, i, j)
         return perimeter
 
-    def count_zero(self, grid, i, j):
-        import itertools
-        zero_nums = 0
-        for x, y in itertools.product([i, i - 1, i + 1], [j, j - 1, j + 1]):
+    def count_edge(self, grid, i, j):
+        edges = 4
+        for x, y in [(i - 1, j), (i + 1, j), (i, j - 1), (i, j + 1)]:
             if x not in range(0, len(grid)) or \
                     y not in range(0, len(grid[0])):
                 continue
-            if x != i and y != j:
-                continue
-            if x == i and y == j:
-                continue
-            if grid[x][y] == 0:
-                zero_nums += 1
-        if i == 0 or i == len(grid) - 1:
-            zero_nums += 1
-        if j == 0 or j == len(grid[0]) - 1:
-            zero_nums += 1
-        return zero_nums
+            if grid[x][y] == 1:
+                edges -= 1
+        return edges
 
 
 if __name__ == '__main__':
