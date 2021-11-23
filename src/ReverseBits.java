6350613f9dc30afb2bd4a31704e3243f76748cd1
flametest <flametest@gmail.com>
2021-11-23 23:04:03 +0800

    190. Reverse Bits: another solution.

--- src/ReverseBits.java
+++ src/ReverseBits.java
@@ -17,3 +17,25 @@ public class ReverseBits {
         System.out.println(r.reverseBits(43261596));
     }
 }
+
+// Another Solution
+// https://leetcode.com/problems/reverse-bits/discuss/1559375/Java-beats-99
+
+//public class Solution {
+//    // you need treat n as an unsigned value
+//    public int reverseBits(int n) {
+//        int result = 0;
+//        int lastBit;
+//        for(int i = 0; i < 32; i++){
+//            // get the last bit of n
+//            lastBit = n & 1;
+//            // shift n for new last bit
+//            n >>= 1;
+//            // shift result so previous last bit is at the front
+//            result <<= 1;
+//            // add result last bit
+//            result |= lastBit;
+//        }
+//        return result;
+//    }
+//}
