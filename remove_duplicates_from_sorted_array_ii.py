01d03439890fbb4bd35aab5276dfa65351a0bd1b
flametest <flametest@gmail.com>
2025-01-08 15:29:00 +0800

    80. Remove Duplicates from Sorted Array II (alternative solution)

--- remove_duplicates_from_sorted_array_ii.py
+++ remove_duplicates_from_sorted_array_ii.py
@@ -15,6 +15,7 @@ class Solution:
         while i < length - 1:
             count = 1
             cur_num = nums[i]
+            # The number at the end should be ignored.
             for j in range(i + 1, length - shift):
                 if nums[j] == cur_num:
                     count += 1
@@ -24,6 +25,7 @@ class Solution:
             if count <= 2:
                 continue
             # else shift the nums
+            # the shift should be added up.
             shift = shift + count - 2
             for k in range(i, length - count + 2):
                 nums[k] = nums[k + count - 2]
@@ -31,8 +33,46 @@ class Solution:
         return len(nums) - shift
 
 
+class Solution1:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n <= 2:
+            return n
+        j = 2
+        for i in range(2, n):
+            if nums[i] != nums[j - 2]:
+                nums[j] = nums[i]
+                j += 1
+        print(nums)
+        return j
+
+
+class Solution2:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        l = r = 0
+        while r < len(nums):
+            count = 1
+            while r + 1 < len(nums) and nums[r] == nums[r + 1]:
+                count += 1
+                r += 1
+            for i in range(min(2, count)):
+                nums[l] = nums[r]
+                l += 1
+            r += 1
+        print(nums)
+        return l
+
+
 if __name__ == '__main__':
     s = Solution()
+    s1 = Solution1()
+    s2 = Solution2()
     print(s.removeDuplicates([1, 1, 1, 2, 2, 3]))
     print(s.removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
     print(s.removeDuplicates([1, 1, 1, 2, 2, 2, 3, 3]))
+    print(s1.removeDuplicates([1, 1, 1, 2, 2, 3]))
+    print(s1.removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
+    print(s1.removeDuplicates([1, 1, 1, 2, 2, 2, 3, 3]))
+    print(s2.removeDuplicates([1, 1, 1, 2, 2, 3]))
+    print(s2.removeDuplicates([0, 0, 1, 1, 1, 1, 2, 3, 3]))
+    print(s2.removeDuplicates([1, 1, 1, 2, 2, 2, 3, 3]))
