01353980fb2e202411325a3ca4176962625d5e19
flametest <flametest@gmail.com>
2025-01-16 22:01:49 +0800

    6. Zigzag Conversion (alternative solution)

--- zigzag_conversion.py
+++ zigzag_conversion.py
@@ -7,11 +7,11 @@
 
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        # zigzag的中间字符 总是比numRows少2
         # 先计算需要多少列
         column = 0
         lengthOfStr = len(s)
         flag = True
+        # zigzag的中间字符 总是比numRows少2
         # 针对这个case s= "AB"
         minus_row = 2 if numRows > 3 else 0
         while lengthOfStr > 0:
@@ -38,6 +38,7 @@ class Solution:
                     i = x
                     if k >= len(s):
                         break
+                # 这里在循环外加减
                 j += 1
                 i -= 1
                 switch_flag = False
@@ -46,6 +47,7 @@ class Solution:
                     if i >= numRows or j >= column:
                         break
                     matrix[i][j] = s[k]
+                    # 这里需要在循环内加减
                     j += 1
                     i -= 1
                     k += 1
@@ -60,10 +62,26 @@ class Solution:
         return "".join(res)
 
 
+# https://www.youtube.com/watch?v=Q2Tw6gcVEwc
+class Solution1:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:
+            return s
+        res = ""
+        for r in range(numRows):
+            increment = 2 * (numRows - 1)
+            for i in range(r, len(s), increment):
+                res += s[i]
+                # middle row
+                if 0 < r < numRows - 1 and i * increment - 2 * r < len(s):
+                    res += s[i * increment - 2 * r]
+        return res
+
+
 if __name__ == '__main__':
     s = Solution()
-    # print(s.convert("PAYPALISHIRING", 3))
-    # print(s.convert("PAYPALISHIRING", 4))
-    # print(s.convert("A", 1))
-    # print(s.convert("AB", 1))
+    print(s.convert("PAYPALISHIRING", 3))
+    print(s.convert("PAYPALISHIRING", 4))
+    print(s.convert("A", 1))
+    print(s.convert("AB", 1))
     print(s.convert("PAYPALISHIRING", 6))
